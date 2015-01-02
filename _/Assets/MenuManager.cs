8b51a814853a72bd9600ef11f77733b8ed1d419a
satirikasha <satirikasha@gmail.com>
2015-01-02 04:46:18 +0400

    Начал настраивать меню

--- _/Assets/MenuManager.cs
+++ _/Assets/MenuManager.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿namespace RagingTribes.Menu {
+  using UnityEngine;
+  using System.Collections;
 
-public class MenuManager : MonoBehaviour {
+  public class MenuManager: MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private GUITexture _Background;
+
+    // Use this for initialization
+    void Start() {
+      Load();   
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void Load() {
+      _Background = this.gameObject.AddComponent<GUITexture>();
+      _Background.texture = Resources.Load<Texture>(ResourcePaths.Background);
+      _Background.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+      _Background.enabled = true;
+    }
+
+    public void Play() {
+      Application.LoadLevelAsync("Game");
+    }
+  }
 }
---
+++ _/Assets/Resources.cs
@@ -0,0 +1,13 @@
+﻿namespace RagingTribes.Menu {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  public static class ResourcePaths {
+    private const string _MainFolder = "Sprites/Menu/";
+
+    public const string Background = _MainFolder + "MenuBackground";
+  }
+}
+
